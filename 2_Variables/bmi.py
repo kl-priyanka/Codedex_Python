603cff22734b71007bd54d4fb1303c2b37f3cbb8
kl-priyanka <priyankz432@gmail.com>
2025-02-10 13:20:43 +0530

    10022025_1

---
+++ 2_Variables/bmi.py
@@ -0,0 +1,15 @@
+#exponents
+
+score = 2 ** 2      # score is 4
+score = 2 ** 3      # score is now 8
+score = 2 ** 4      # score is now 16
+score = 2 ** 5      # score is now 32
+
+print(score)        # Output: 32
+
+#bmi = mass / (height ** 2)
+#mass is in kilograms and height is in meters
+mass = 69
+height = 1.65
+bmi = mass / (height ** 2)
+print(bmi) # Output: 25.34
