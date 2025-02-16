29a1198e88345714e363d39a821eb1209ab5ad5e
kl-priyanka <priyankz432@gmail.com>
2025-02-16 13:20:13 +0530

    16022025_1

---
+++ 2_Variables/the_cyclone.py
@@ -0,0 +1,9 @@
+height = print("Enter your height in cms: ")
+credits = print("Enter the number of credits you have: ")
+
+if height >= 137 and credits >= 10:
+    print("Enjoy the ride!")
+elif credits >= 10:
+    print("You are not tall enough to ride.")
+elif height >= 137:
+    print("You don't have enough credits")
